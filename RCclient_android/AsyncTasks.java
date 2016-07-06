6b6a8d9e8539dc31ef848e00daba676d1ab06d9e
Zhuravlev Sergey <Sergey.Zhuravlev@advancedmonitoring.ru>
2016-07-06 09:44:01 +0300

    init android

---
+++ RCclient_android/AsyncTasks.java
@@ -0,0 +1,237 @@
+package com.example.jura.myapplication;
+
+import android.os.AsyncTask;
+import android.os.SystemClock;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+/**
+ * Created by Jura on 11.06.2016.
+ */
+public class AsyncTasks {
+    final static int SERVER_CODE_LIST = 1;
+    final static int SERVER_CODE_DURATION = 2;
+    final static int SERVER_CODE_POSITION = 3;
+    final static int SERVER_CODE_PLAYBACK = 4;
+
+    static class HttpGetResult {
+        String message;
+        int code;
+        HttpGetResult(int xcode, String xmessage) {
+            code = xcode;
+            message = xmessage;
+        }
+    }
+
+    private static String readStream(InputStream is) {
+        try {
+            ByteArrayOutputStream bo = new ByteArrayOutputStream();
+            int i = is.read();
+            while (i != -1) {
+                bo.write(i);
+                i = is.read();
+            }
+            return bo.toString();
+        } catch (IOException e) {
+            return "";
+        }
+    }
+
+    public static class HttpGetCookie extends AsyncTask<Object, String, List<String>> {
+        JuraActivity mainClass;
+
+        @Override
+        protected List<String> doInBackground(Object... params) {
+            String url = (String) params[0];
+            mainClass = (JuraActivity) params[1];
+
+            URL net_url;
+            HttpURLConnection urlConnection;
+            List<String> cookies;
+            String cookie;
+            List<String> res = new ArrayList<String>();
+
+            try {
+                net_url = new URL(url);
+            } catch (MalformedURLException e) {
+                res.add("exception");
+                res.add(e.toString()+url);
+                return res;
+            }
+
+            try {
+                urlConnection = (HttpURLConnection) net_url.openConnection();
+                urlConnection.setConnectTimeout(10*1000);
+                urlConnection.connect();
+                cookies = urlConnection.getHeaderFields().get("Set-Cookie");
+                urlConnection.disconnect();
+
+                if (cookies == null) {
+                    res.add("exception");
+                    res.add("cookies are null"+url);
+                    return res;
+                }
+
+                if (cookies.isEmpty()) {
+                    res.add("exception");
+                    res.add("cookies emty"+url);
+                    return res;
+                }
+                cookie = cookies.get(0).split(";")[0];
+                if ((cookie.length() < 5) && !cookie.substring(0,3).equals("user")) {
+                    res.add("exception");
+                    res.add("malformed cookie"+url);
+                    return res;
+                }
+            } catch (IOException e) {
+                res.add("exception");
+                res.add(e.toString()+url);
+                return res;
+            }
+
+            res.add(url);
+            res.add(cookie);
+            return res;
+        }
+
+        protected void onPostExecute(List<String> result) {
+            mainClass.setWorkStatus(false);
+            if (result.isEmpty())
+                return;
+            if (result.get(0).equals("exception")) {
+                //mainClass.setDebugView(result.get(1));
+                return;
+            }
+            mainClass.setTextView("got cookie"+result.get(1));
+            mainClass.setParams(result);
+            //got cookie, now get root dir
+            mainClass.startAsyncTask("app");
+        }
+    }
+
+    public static class HttpGet extends AsyncTask<Object, String, HttpGetResult> {
+        JuraActivity mainClass;
+
+        @Override
+        protected HttpGetResult doInBackground(Object... params) {
+            String url = (String)params[0];
+            String appCookie = (String)params[1];
+            mainClass = (JuraActivity) params[2];
+
+            URL net_url;
+            HttpURLConnection urlConnection;
+            String s = "didn't trigger";
+            int code = 0;
+
+            try {
+                net_url = new URL(url);
+            } catch (MalformedURLException e) {
+                return new HttpGetResult(1, e.toString());
+            }
+
+            try {
+                urlConnection = (HttpURLConnection) net_url.openConnection();
+                urlConnection.addRequestProperty("Cookie", appCookie);
+                urlConnection.connect();
+                code = urlConnection.getResponseCode();
+                if (code == 401) {
+                    urlConnection.disconnect();
+                    return new HttpGetResult(code, "");
+                }
+            } catch (IOException e) {
+                return new HttpGetResult(1, e.toString());
+            }
+
+            try {
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                s = readStream(in);
+                publishProgress(s);
+                urlConnection.disconnect();
+            } catch (IOException e) {
+                urlConnection.disconnect();
+                return new HttpGetResult(1, e.toString());
+            }
+
+            return new HttpGetResult(code, s);
+        }
+        protected void onProgressUpdate(String... progress) {
+            if (progress[0].length() == 0){}
+                //mainClass.setDebugView("empty string!");
+            //mainClass.setDebugView(progress[0]);
+        }
+
+        protected void onPostExecute(HttpGetResult result) {
+            mainClass.setWorkStatus(false);
+            if (result.code == 401) {
+                //cookie wasn't found on the server (restarted), get new cookie
+                mainClass.startAsyncTaskCookie();
+            }
+            mainClass.setDebugView(result.message);
+            if (result.message.isEmpty()) {
+                mainClass.setDebugView("Empty!");
+                return;
+            }
+            try {
+                JSONObject reader = new JSONObject(result.message);
+                //mainClass.setDebugView(reader.getString("code"));
+                int code = reader.getInt("code");
+                mainClass.setDebugView(String.valueOf(code));
+                switch (code) {
+                    case SERVER_CODE_DURATION:
+                        mainClass.setDuration(reader.getInt("duration"));
+                        mainClass.setDebugView(Integer.toString(reader.getInt("duration")));
+                        break;
+                    case SERVER_CODE_LIST:
+                        mainClass.buildTable(reader.getString("list"));
+                        break;
+                    case SERVER_CODE_POSITION:
+                        //mainClass.buildTable(reader.getInt("position"));
+                        break;
+                    case SERVER_CODE_PLAYBACK:
+                        mainClass.setSeekBarProgress();
+                        mainClass.startAsyncTaskDuration();
+                        break;
+
+                }
+            } catch (JSONException e) {
+                mainClass.setDebugView(e.toString());
+                return;
+            }
+
+
+
+            /*else if (result.code == 201) {
+                mainClass.buildTable(result.message);
+            }*/
+
+        }
+    }
+
+    public static class GetDuration extends AsyncTask<Object, Void, Void> {
+        JuraActivity mainClass;
+
+        @Override
+        protected Void doInBackground(Object... params) {
+            mainClass = (JuraActivity) params[0];
+            SystemClock.sleep(1000);
+            return null;
+        }
+
+        protected void onPostExecute(Void result) {
+            mainClass.setWorkStatus(false);
+            mainClass.startAsyncTask("duration");
+        }
+    }
+}
---
+++ RCclient_android/JuraActivity.java
@@ -0,0 +1,180 @@
+package com.example.jura.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.view.View;
+import android.util.TypedValue;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.TableRow;
+import android.widget.TableLayout;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.net.URLEncoder;
+
+public class JuraActivity extends AppCompatActivity {
+
+    private TextView debugView;
+    private TextView textView;
+    private TableLayout tableLayout;
+    private SeekBar seekBar;
+    private SeekBarListener seekBarListener;
+
+
+    private String serverIp = "";
+    private String appCookie = "";
+    private boolean inWork = false;
+
+    private int duration = 0;
+
+    //public interaction
+    public void setParams(List<String> params) {
+        serverIp = params.get(0);
+        appCookie = params.get(1);
+    }
+    public String getServerIp() { return serverIp; }
+    public void setDuration(int seconds) { duration = seconds; }
+    public void setWorkStatus(boolean status) {
+        inWork = status;
+        if(status)
+            textView.setText("in work");
+        else
+            textView.setText("not in work");
+
+    }
+    public void setDebugView(String s) {
+        debugView.setText(s);
+    }
+    public void setTextView(String s) {
+        //textView.setText(textView.getText().toString() + '\t'+ s);
+    }
+    public void setSeekBarProgress() {
+        seekBar.setProgress(0);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jura);
+
+        tableLayout = (TableLayout) findViewById(R.id.tableLayout);
+        tableLayout.setColumnStretchable(0, true);
+        //buildTable("");
+
+        seekBar =  (SeekBar) findViewById(R.id.seekBar);
+        seekBarListener = new SeekBarListener();
+        seekBarListener.init(this);
+        seekBar.setOnSeekBarChangeListener(seekBarListener);
+
+        //debug
+        debugView = (TextView) findViewById(R.id.textView2);
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setOnClickListener(debug);
+        textView.setClickable(true);
+        //
+
+        findServer();
+    }
+    private void findServer() {
+        serverIp = "http://192.168.199.14:5000";
+        startAsyncTaskCookie();
+
+        /*
+        StringBuilder address = new StringBuilder("http://192.168.199.10:5000");
+        for (int i = 0; i < 10; i++) {
+            address.setCharAt(20, (char)(0x30+i));
+            AsyncTasks.HttpGetCookie httpGetCookie = new AsyncTasks.HttpGetCookie();
+            httpGetCookie.execute(address.toString(), this);
+        }*/
+    }
+
+    private View.OnClickListener fsClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            TextView tv = (TextView)v;
+            String s = tv.getText().toString();
+            String arg;
+
+            try {
+                arg = URLEncoder.encode(s, "utf-8");
+                startAsyncTask("app?arg="+arg);
+            } catch(UnsupportedEncodingException e) {
+
+            }
+        }
+    };
+
+    View.OnClickListener debug = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startAsyncTask("duration");
+        }
+    };
+
+    void buildTable(String serverResponse) {
+        tableLayout.removeAllViews();
+
+        String[] entities = serverResponse.split("\n");
+        for (int i = 0; i < entities.length+1; i++) {
+            TableRow row = new TableRow(this);
+            row.setMinimumHeight(90);
+            TextView tv = new TextView(this);
+            tv.setTextSize(22);
+            if (i == 0)
+                tv.setText("..");
+            else
+                tv.setText(entities[i-1]);
+            tv.setOnClickListener(fsClick);
+
+            //ripple effect
+            TypedValue outValue = new TypedValue();
+            tv.getContext().getTheme().resolveAttribute(android.R.attr.selectableItemBackground, outValue, true);
+            tv.setBackgroundResource(outValue.resourceId);
+
+            row.addView(tv);
+            tableLayout.addView(row,i);
+        }
+    }
+
+    /////////// API /////////////
+
+    public void startAsyncTask(String url) {
+        if (inWork)
+            return;
+        setWorkStatus(true);
+        AsyncTasks.HttpGet httpGet = new AsyncTasks.HttpGet();
+        httpGet.execute(serverIp+"/"+url, appCookie, this);
+    }
+    public void startAsyncTaskCookie() {
+        if (inWork)
+            return;
+        setWorkStatus(true);
+        AsyncTasks.HttpGetCookie httpGetCookie = new AsyncTasks.HttpGetCookie();
+        httpGetCookie.execute(serverIp, this);
+    }
+    public void startAsyncTaskDuration() {
+        if (inWork)
+            return;
+        setWorkStatus(true);
+        AsyncTasks.GetDuration getDuration = new AsyncTasks.GetDuration();
+        getDuration.execute(this);
+    }
+    public void serverKillAll(View v) {
+        startAsyncTask("killall");
+        duration = 0;
+    }
+    public void serverPause(View v) {
+        startAsyncTask("pause");
+    }
+    public void setPosition(int pos) {
+        if (duration == 0)
+            return;
+        int newPos = duration * pos;
+        startAsyncTask("setposition?arg="+Integer.toString(newPos)+"0000");
+    }
+    public void Sync(View v) {
+        findServer();
+    }
+}
